2220a9af8708f0c05bb8c0f0f8e21477ae00e597
Gcreach <gcreach97@gmail.com>
2023-05-01 20:10:39 -0700

    change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,4 @@
-// movie api request
-var requestMovieUrl = "http://www.omdbapi.com/?apikey=c6fae28f&s=avatar";
 
-function movieApi() {
-  fetch(requestMovieUrl)
-    .then(function (response) {
-      console.log(response);
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
-}
-
-movieApi();
-
-//event lister for search button that adds text box content to the request url
-var searchBtn = document.querySelector("#btn");
-var searchInput = document.querySelector
-
-
-
-searchBtn.addEventListener("click", movieApi);
-// create funtion to print results
-
-//vars for results that create elements to display results
-//
 var searchBtn = document.getElementById('btn');
 var form = $('#form');
 
